f7b8dc11b43d9cd9670812d9f603697a1dda3672
James <tsunyoku@gmail.com>
2023-10-22 20:07:40 +0100

    Update dockerization & pull secrets from vault (#1)
    * ya
    
    * move pool max size into env vars
    
    * feat: add deploy CI
    
    * fix: master -> main
    
    * fix pt2
    
    * feat: add dockerignore
    
    * fix: install pip
    
    * fix: apt-get -> apt
    
    Co-authored-by: Josh Smith <cmyuiosu@gmail.com>
    
    ---------
    
    Co-authored-by: Josh Smith <cmyuiosu@gmail.com>

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [ -z "$APP_ENV" ]; then
+  echo "Please set APP_ENV"
+  exit 1
+fi
+
+if [ -z "$APP_COMPONENT" ]; then
+  echo "Please set APP_COMPONENT"
+  exit 1
+fi
+
+if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
+  pip install --break-system-packages -i $PYPI_INDEX_URL akatsuki-cli
+  akatsuki vault get performance-service $APP_ENV -o .env
+  source .env
+fi
+
+# await database availability
+/scripts/await-service.sh $DATABASE_HOST $DATABASE_PORT $SERVICE_READINESS_TIMEOUT
+
+# await redis availability
+/scripts/await-service.sh $REDIS_HOST $REDIS_PORT $SERVICE_READINESS_TIMEOUT
+
+# await amqp availability
+/scripts/await-service.sh $AMQP_HOST $AMQP_PORT $SERVICE_READINESS_TIMEOUT
+
+# run the service (APP_COMPONENT is handled by the service)
+/usr/local/bin/performance-service
--- src/config.rs
+++ src/config.rs
@@ -7,13 +7,52 @@ pub struct Config {
     pub api_port: Option<u16>,
 
     #[clap(long, env)]
-    pub database_url: String,
+    pub database_host: String,
 
     #[clap(long, env)]
-    pub amqp_url: String,
+    pub database_port: u16,
 
     #[clap(long, env)]
-    pub redis_url: String,
+    pub database_username: String,
+
+    #[clap(long, env)]
+    pub database_password: String,
+
+    #[clap(long, env)]
+    pub database_name: String,
+
+    #[clap(long, env)]
+    pub database_pool_max_size: usize,
+
+    #[clap(long, env)]
+    pub amqp_host: String,
+
+    #[clap(long, env)]
+    pub amqp_port: u16,
+
+    #[clap(long, env)]
+    pub amqp_username: String,
+
+    #[clap(long, env)]
+    pub amqp_password: String,
+
+    #[clap(long, env)]
+    pub amqp_pool_max_size: usize,
+
+    #[clap(long, env)]
+    pub redis_host: String,
+
+    #[clap(long, env)]
+    pub redis_port: u16,
+
+    #[clap(long, env)]
+    pub redis_username: Option<String>,
+
+    #[clap(long, env)]
+    pub redis_password: Option<String>,
+
+    #[clap(long, env)]
+    pub redis_database: i64,
 
     #[clap(long, env)]
     pub beatmaps_path: String,
