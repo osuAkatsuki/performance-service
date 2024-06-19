2cd9bb74dfa8bc5f2235e1d8a7667353ef859eaa
cmyui <cmyuiosu@gmail.com>
2024-06-18 22:36:28 -0400

    temp fix/disable s3 usage for perf calculation

--- src/usecases/beatmaps.rs
+++ src/usecases/beatmaps.rs
@@ -1,33 +1,33 @@
 use std::sync::Arc;
 
-use s3::error::S3Error;
+// use s3::error::S3Error;
 
 use crate::context::Context;
 
 pub async fn fetch_beatmap_osu_file(
     beatmap_id: i32,
-    beatmap_md5: &str,
-    context: Arc<Context>,
+    _beatmap_md5: &str,
+    _context: Arc<Context>,
 ) -> anyhow::Result<Vec<u8>> {
-    let beatmap_path = &format!("beatmaps/{beatmap_id}.osu");
+    // let beatmap_path = &format!("beatmaps/{beatmap_id}.osu");
 
-    let existing_file = match context.bucket.get_object(beatmap_path).await {
-        Ok(existing_file) => Ok(Some(existing_file)),
-        Err(S3Error::Http(status_code, _)) if status_code == 404 => Ok(None),
-        Err(e) => Err(e),
-    }?;
+    // let existing_file = match context.bucket.get_object(beatmap_path).await {
+    //     Ok(existing_file) => Ok(Some(existing_file)),
+    //     Err(S3Error::Http(status_code, _)) if status_code == 404 => Ok(None),
+    //     Err(e) => Err(e),
+    // }?;
 
-    match existing_file {
-        Some(existing_file) => {
-            let osu_file_data = existing_file.to_vec();
-            let osu_file_data_md5 = format!("{:x}", md5::compute(&osu_file_data));
+    // match existing_file {
+    //     Some(existing_file) => {
+    //         let osu_file_data = existing_file.to_vec();
+    //         let osu_file_data_md5 = format!("{:x}", md5::compute(&osu_file_data));
 
-            if osu_file_data_md5 == beatmap_md5 {
-                return Ok(osu_file_data);
-            }
-        }
-        None => {}
-    }
+    //         if osu_file_data_md5 == beatmap_md5 {
+    //             return Ok(osu_file_data);
+    //         }
+    //     }
+    //     None => {}
+    // }
 
     let osu_response = reqwest::get(&format!("https://old.ppy.sh/osu/{beatmap_id}"))
         .await?
@@ -35,10 +35,10 @@ pub async fn fetch_beatmap_osu_file(
 
     let response_bytes = osu_response.bytes().await?.to_vec();
 
-    context
-        .bucket
-        .put_object(beatmap_path, &response_bytes)
-        .await?;
+    // context
+    //     .bucket
+    //     .put_object(beatmap_path, &response_bytes)
+    //     .await?;
 
     Ok(response_bytes)
 }
