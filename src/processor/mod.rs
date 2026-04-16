d9de4888c07d3531bbf58ed190d06e20abb246c4
Julius <41292439+infernalfire72@users.noreply.github.com>
2026-04-16 23:39:05 +0200

    Update mod.rs

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -257,6 +257,31 @@ async fn calculate_everything_at_once_pp(
     Ok(pp)
 }
 
+async fn calculate_kippy_attempt_pp(
+    score: &RippleScore,
+    context: Arc<Context>,
+) -> anyhow::Result<f32> {
+    let beatmap_bytes =
+        usecases::beatmaps::fetch_beatmap_osu_file(score.beatmap_id, context).await?;
+    let beatmap = kippy_attempt::Beatmap::from_bytes(&beatmap_bytes)?;
+
+    let result = kippy_attempt::osu_2019::OsuPP::from_map(&beatmap)
+        .mods(score.mods as u32)
+        .combo(score.max_combo as u32)
+        .n300(score.count_300 as u32)
+        .n100(score.count_100 as u32)
+        .n50(score.count_50 as u32)
+        .misses(score.count_misses as u32)
+        .calculate();
+
+    let mut pp = round(result.pp as f32, 2);
+    if pp.is_infinite() || pp.is_nan() {
+        pp = 0.0;
+    }
+
+    Ok(pp)
+}
+
 async fn process_scores(
     rework: &Rework,
     scores: Vec<RippleScore>,
