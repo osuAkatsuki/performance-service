4fc2863c29b58d010abe287b62f70bfbd2361c1b
kip <99145984+kippysevenstars@users.noreply.github.com>
2026-04-16 17:25:36 -0400

    Asking the real questions

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -275,6 +275,7 @@ async fn process_scores(
             26 => calculate_aim_accuracy_fix_pp(score, context.clone()).await?,
             27 => calculate_improved_miss_penalty_and_acc_rework_pp(score, context.clone()).await?,
             28 => calculate_everything_at_once_pp(score, context.clone()).await?,
+            29 => calculate_kippy_attempt_pp(score, context.clone()).await?,
             _ => unreachable!(),
         };
 
